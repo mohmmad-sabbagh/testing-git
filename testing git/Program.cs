817f5edb14924b7cd1a689dbe1ea4a73c33df7e6
mohmmad-sabbagh <131598577+mohmmad-sabbagh@users.noreply.github.com>
2023-08-23 01:10:22 +0300

    Update Program.cs

--- testing git/Program.cs
+++ testing git/Program.cs
@@ -10,7 +10,7 @@ namespace testing_git
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world");
+            Console.WriteLine("hello world of fools");
         }
     }
 }
